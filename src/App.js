3ddb5ae2174046cd3014920d051070cd6b4b7abe
Nirman529 <68377229+Nirman529@users.noreply.github.com>
2023-07-07 13:54:14 +0530

    HOC concept using api call for todo list and comments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Counter1 from "./components/Counter1";
+import Counter2 from "./components/Counter2";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App row m-0">
+        <div className="col">
+          <Counter1 />
+        </div>
+        <div className="col">
+          <Counter2 />
+        </div>
+      </div>
+    </>
   );
 }
 
